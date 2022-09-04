d7b066c4bb6803e523ce38967407a1bd99b561e1
Sierra Nutt <sierra.nutt@outlook.com>
2022-09-03 20:10:47 -0600

    newest changes

--- Main/routes/apiRoutes.js
+++ Main/routes/apiRoutes.js
@@ -1,20 +1,21 @@
-const router = require('express').Router();
-const store = require('../db/store');
+export default function Project() {
+  const routers = require('express').Router();
+  const store = require('../db/store');
 
-router.get('/notes', (req, res) => {
-  store
-    .showNotes()
-    .then((notes) => {
-      return res.json(notes);
-    })
-    .catch((err) => res.status(500).json(err));
-});
+  routers.get('/notes', (req, res) => {
+    store
+      .getNotes()
+      .then((notes) => {
+        return res.json(notes);
+      })
+      .catch((err) => res.status(500).json(err));
+  });
 
-router.post('/notes', (req, res) => {
-  store
-    .addNote(req.body)
-    .then((note) => res.json(note))
-    .catch((err) => res.status(500).json(err));
-});
+  routers.post('/notes', (req, res) => {
+    store
+      .addNote(req.body)
+      .then((note) => res.json(note))
+      .catch((err) => res.status(500).json(err));
+  });
 
-module.exports = router;
+}
